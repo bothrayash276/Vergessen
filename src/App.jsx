00d53ff8d88b67d2b0830f01f316e7b4e741cac6
bothrayash276 <bothrayash276@gmail.com>
2026-06-10 22:03:15 +0530

    repo initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <span
+      className='bg-red-500'>
+        Hello
+      </span>
+    </div>
+  )
+}
+
+export default App
